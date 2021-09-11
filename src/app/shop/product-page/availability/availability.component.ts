65205287a84264562ec66fdbfaa0188e7b2e6918
Habib Mokni <habib.mokni@outlook.fr>
2021-09-11 23:08:03 +0500

    Added missing components
    Shop and maps component have been added.

---
+++ src/app/shop/product-page/availability/availability.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-availability',
+  templateUrl: './availability.component.html',
+  styleUrls: ['./availability.component.css']
+})
+export class AvailabilityComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
